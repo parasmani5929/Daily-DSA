4ec6bbd58c1ff60421c33c62b87ae86db01e26a0
Paras Mani <168018735+parasmani5929@users.noreply.github.com>
2025-09-06 22:45:21 +0530

    Update maxSum_array.cpp

--- maxSum_array.cpp
+++ maxSum_array.cpp
@@ -1,2 +1,21 @@
 #include <stdio.h>
 #include <vector.h>
+using namespace std;
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+
+        for (int val : nums){
+            int freq = 0;
+            for(int key : nums){
+            if(val == key){
+                freq++;
+            }
+        }
+        if(freq >= n/2){
+            return val;
+        }
+        }
+        return 0;
+    };
