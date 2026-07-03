3ca48e18b89cfce931b7a7af863f7aa789537c0d
Paras Mani <168018735+parasmani5929@users.noreply.github.com>
2026-07-03 11:15:37 +0530

    Update remove_val.cpp

--- remove_val.cpp
+++ remove_val.cpp
@@ -3,7 +3,7 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int n = nums.size();
+        int n = nums.size(); 
         int k = 0;
 
         for(int i = 0; i < n; i++){
